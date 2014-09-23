e4a1b9e28a112ea4a5a4aa06740f0aa3ce852662
Saulius Grigaliunas <saulius@ninja.lt>
2014-09-23 12:56:09 +0300

    Add ToFloat morphline

---
+++ camus-sweeper/src/main/java/com/vinted/camus/sweeper/morphlines/commands/ToFloatBuilder.java
@@ -0,0 +1,48 @@
+package org.kitesdk.morphline.stdlib;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.ListIterator;
+
+import org.kitesdk.morphline.api.Command;
+import org.kitesdk.morphline.api.CommandBuilder;
+import org.kitesdk.morphline.api.MorphlineContext;
+import org.kitesdk.morphline.api.Record;
+import org.kitesdk.morphline.base.AbstractCommand;
+
+import com.typesafe.config.Config;
+
+public final class ToFloatBuilder implements CommandBuilder {
+
+  @Override
+  public Collection<String> getNames() {
+    return Collections.singletonList("toFloat");
+  }
+
+  @Override
+  public Command build(Config config, Command parent, Command child, MorphlineContext context) {
+    return new ToFloat(this, config, parent, child, context);
+  }
+
+  private static final class ToFloat extends AbstractCommand {
+    private final String fieldName;
+
+    public ToFloat(CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context) {
+      super(builder, config, parent, child, context);
+      this.fieldName = getConfigs().getString(config, "field");
+      validateArguments();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected boolean doProcess(Record record) {
+      ListIterator iter = record.get(fieldName).listIterator();
+      while (iter.hasNext()) {
+        String str = iter.next().toString();
+        iter.set(new Float(str));
+      }
+
+      return super.doProcess(record);
+    }
+  }
+}
